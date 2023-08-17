f8f7d93130bd3d832cce54030a148ac10ba1fda8
Juan Diego Rodríguez Guarín <minionpaul85@gmail.com>
2023-08-17 17:15:39 -0500

    Teste controladroes vehiculos

--- test/vehiculoControllers.test.js
+++ test/vehiculoControllers.test.js
@@ -1,19 +1,52 @@
 const db = require('../services/db');
-const {obtener_vehiculos,  obtener_vehiculos_id, crear_vehiculo, actualizar_vehiculo, eliminar_vehiculo} = require('../controllers/VehiculoControllers');
+const { obtener_vehiculos, eliminar_vehiculo } = require('../controllers/VehiculoControllers');
+const assert = require('assert');
+
+jest.mock('../services/db');
 
 describe('obtener_vehiculos', () => {
 
-    // Tests that the function returns all artists from the database
-    it('Debe devoler todos los vehiculos de la base de datos', async () => {
-      const mockResult = { rows: ['Vehiculo1', 'Vehiculo2'] };
-      db.execute = jest.fn().mockResolvedValue(mockResult);
-      const mockJson = jest.fn();
-      const res = { json: mockJson };
+  it('Debe devoler todos los vehiculos de la base de datos', async () => {
+    const mockResult = { rows: ['Vehiculo1', 'Vehiculo2'] };
+    db.execute = jest.fn().mockResolvedValue(mockResult);
+    const mockJson = jest.fn();
+    const res = { json: mockJson };
 
-      await obtener_vehiculos({}, res);
+    await obtener_vehiculos({}, res);
 
-      expect(db.execute).toHaveBeenCalledWith('SELECT * FROM Vehiculo');
-      expect(mockJson).toHaveBeenCalledWith(mockResult.rows);
-    });
+    expect(db.execute).toHaveBeenCalledWith('SELECT * FROM Vehiculo');
+    expect(mockJson).toHaveBeenCalledWith(mockResult.rows);
+  });
+});
 
-})
+
+describe('eliminar_vehiculo', () => {
+  it('debería eliminar un vehículo por su ID', async () => {
+    const mockVehiculoId = 1;
+
+    // Simula la función execute de la base de datos
+    const db = {
+      execute: async (query) => {
+        assert.ok(query.includes(`DELETE FROM Vehiculo WHERE id = '${mockVehiculoId}'`));
+      }
+    };
+
+    const req = {
+      params: {
+        id: mockVehiculoId
+      }
+    };
+    let statusCode = null;
+
+    const res = {
+      status: (code) => {
+        statusCode = code;
+        return res;
+      },
+      json: () => { }
+    };
+
+    await eliminar_vehiculo(req, res);
+    assert.strictEqual(statusCode, 200);
+  });
+});
