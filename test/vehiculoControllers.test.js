9124b20ffca93ccf036f1ac38c5119e8dc0962cb
Juan Diego Rodríguez Guarín <minionpaul85@gmail.com>
2023-08-17 17:38:10 -0500

    Test crear vehiculo

--- test/vehiculoControllers.test.js
+++ test/vehiculoControllers.test.js
@@ -1,24 +1,70 @@
 const db = require('../services/db');
-const { obtener_vehiculos, eliminar_vehiculo } = require('../controllers/VehiculoControllers');
+const { obtener_vehiculos, eliminar_vehiculo, crear_vehiculo } = require('../controllers/VehiculoControllers');
 const assert = require('assert');
 
-jest.mock('../services/db');
+jest.mock('../services/db'); // Simula el módulo de base de datos con una versión mock
 
 describe('obtener_vehiculos', () => {
 
   it('Debe devoler todos los vehiculos de la base de datos', async () => {
+
+    // Define el resultado simulado de la consulta a la base de datos
     const mockResult = { rows: ['Vehiculo1', 'Vehiculo2'] };
+
+    // Simula la función execute de la base de datos para devolver el resultado simulado
     db.execute = jest.fn().mockResolvedValue(mockResult);
+
+    // Simula el método json del objeto de respuesta
     const mockJson = jest.fn();
     const res = { json: mockJson };
 
+    // Llama al controlador obtener_vehiculos con los objetos de solicitud y respuesta simulados
     await obtener_vehiculos({}, res);
 
+    // Verifica que la función execute de la base de datos se llamó con la consulta correcta
     expect(db.execute).toHaveBeenCalledWith('SELECT * FROM Vehiculo');
+
+    // Verifica que el método json del objeto de respuesta se llamó con los datos simulados
     expect(mockJson).toHaveBeenCalledWith(mockResult.rows);
   });
 });
 
+describe('crear_vehiculo', () => {
+  it('debería crear un vehículo', async () => {
+    const mockRequestBody = {
+      marca: 'Toyota',
+      cilindraje: 2000,
+      combustible: 'Gasolina',
+      ano: 2022
+    };
+
+    // Simula la función execute de la base de datos para verificar el query INSERT
+    const mockDbExecute = require('../services/db').execute;
+    mockDbExecute.mockResolvedValue(); // Puedes ajustar esto según lo necesites
+
+    // Crea un objeto de solicitud simulado con los datos del vehículo a crear
+    const req = {
+      body: mockRequestBody
+    };
+    let statusCode = null;
+
+    // Simula el objeto de respuesta para verificar el código de estado
+    const res = {
+      status: (code) => {
+        statusCode = code;
+        return res;
+      },
+      json: () => { }
+    };
+
+    // Llama al controlador crear_vehiculo con los objetos de solicitud y respuesta simulados
+    await crear_vehiculo(req, res);
+
+    // Verifica que la función execute de la base de datos se haya llamado y que el código de estado sea 201
+    expect(mockDbExecute).toHaveBeenCalled();
+    expect(statusCode).toBe(201);
+  });
+});
 
 describe('eliminar_vehiculo', () => {
   it('debería eliminar un vehículo por su ID', async () => {
@@ -27,6 +73,7 @@ describe('eliminar_vehiculo', () => {
     // Simula la función execute de la base de datos
     const db = {
       execute: async (query) => {
+        // Verifica que el query DELETE tenga el ID correcto
         assert.ok(query.includes(`DELETE FROM Vehiculo WHERE id = '${mockVehiculoId}'`));
       }
     };
@@ -38,6 +85,7 @@ describe('eliminar_vehiculo', () => {
     };
     let statusCode = null;
 
+    // Simula el objeto de respuesta
     const res = {
       status: (code) => {
         statusCode = code;
@@ -47,6 +95,8 @@ describe('eliminar_vehiculo', () => {
     };
 
     await eliminar_vehiculo(req, res);
+
+    // Verifica que el código de estado sea 200 para eliminación exitosa
     assert.strictEqual(statusCode, 200);
   });
 });
